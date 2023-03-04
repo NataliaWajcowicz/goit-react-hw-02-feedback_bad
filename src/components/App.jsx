66af13059906b064ade36f333986c4981b91edde
NataliaWajcowicz <n.wajcowicz@o2.pl>
2023-03-04 15:14:46 +0100

    notifications

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Notifications } from './Notifications/Notifications';
 import { Statistics } from './Statistics/Statistics';
 
 export class App extends Component {
@@ -6,8 +7,6 @@ export class App extends Component {
     good: 0,
     neutral: 0,
     bad: 0,
-    goodBtn: 'Good',
-    badBtn: 'Bad',
   };
   addFeedbackGood = () => {
     this.setState(state => ({ good: state.good + 1 }));
@@ -42,26 +41,29 @@ export class App extends Component {
         <ul>
           <li>
             <button
-            onClick={this.addFeedbackGood}>{this.state.goodBtn}</button>
+              onClick={this.addFeedbackGood}>Good</button>
           </li>
           <li>
             <button
-            onClick={this.addFeedbackNeutral}>Neutral</button>
+              onClick={this.addFeedbackNeutral}>Neutral</button>
           </li>
           <li>
             <button
-            onClick={this.addFeedbackBad}>{this.state.badBtn}</button>
+              onClick={this.addFeedbackBad}>Bad</button>
           </li>
         </ul>
-        <Statistics
-          statisticsTitle={'Statistics'}
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-          total={this.countTotalFeedback()}
-          percentage={this.countPositiveFeedbackPercentage()} />
+        {this.countTotalFeedback() === 0
+          ? (<Notifications/>)
+          : (<Statistics
+            statisticsTitle={'Statistics'}
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            percentage={this.countPositiveFeedbackPercentage()} />
+          )}
       </div>
-    )
+    );
   }
 
 
